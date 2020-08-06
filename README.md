ed5886f0471cef16db49c6b3584822a353f376b2
Chris K <ikim1991@gmail.com>
2020-08-05 20:44:34 -0400

    Initializing Project

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Golf Tracker API
---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}...`)
+})
